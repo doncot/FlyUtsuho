d2f7bf957cafcb19d3265b83344e9cc961a1fe5b
doncot <doncot@gmail.com>
2014-07-05 10:29:16 +0900

    update shooteractor

--- ShooterActor.cpp
+++ ShooterActor.cpp
@@ -1,15 +1,30 @@
 #include"ShooterElements.h"
 
+namespace
+{
+	Inferno::Vec2<int> hitStartPoint;
+}
+
 namespace Inferno
 {
-
 void ShooterActor::Hit()
 {
 	if (m_curState == GEState::ge_neutral)
 	{
 		m_curState = GEState::ge_damage;
 		m_timer.Start(1000);
+		hitStartPoint = m_pos;
 	}
+
+
+
+
+
+
+
+
+
+
 }
 
 void ShooterActor::Update() //これは派生クラスで定義するべきだな……
@@ -21,7 +36,7 @@ void ShooterActor::Update() //これは派生クラスで定義するべきだな……
 		{
 			m_curState = GEState::ge_neutral;
 		}
-
+		
 	}
 }
 

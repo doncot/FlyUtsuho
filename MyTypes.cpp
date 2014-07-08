db8b9e31f93add4ee0faf3d26d43e5dc4d0ee064
doncot <doncot@gmail.com>
2014-07-08 17:15:47 +0900

    MatrixUtility.cppをMytypes.cppに結合

--- MyTypes.cpp
+++ MyTypes.cpp
@@ -2,6 +2,41 @@
 
 namespace Inferno
 {
+void Matrix::LoadIdentity()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == j)
+				m[i][j] = 1;
+			else
+				m[i][j] = 0;
+		}
+	}
+}
+
+void Matrix::Transform2D(const float x, const float y)
+{
+	m[3][0] += x;
+	m[3][1] += y;
+}
+
+const D3DMATRIX Matrix::GetD3DMatrix() const
+{
+	D3DMATRIX dm;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			dm.m[i][j] = m[i][j];
+		}
+	}
+
+	return dm;
+}
+
 Rect::Rect() :m_height(0),m_width(0){}
 
 Rect::Rect(const int height, const int width)

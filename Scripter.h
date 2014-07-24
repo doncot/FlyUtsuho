09e996cbd0168f960bfe731d5ba863f6205d2a10
doncot <doncot@gmail.com>
2014-07-25 04:29:40 +0900

    charで読み込んでいた内容を消去

--- Scripter.h
+++ Scripter.h
@@ -9,6 +9,9 @@ Scripter.h
 #include<codecvt>
 #include<regex>
 #include<string>
+#include<cstdlib>
+#include"Inferno\TString.h"
+#include"ShooterScene.h"
 #include"Inferno\MyException.h"
 
 namespace
@@ -21,6 +24,11 @@ namespace
 		return std::regex_match(str, pattern);
 	}
 
+	bool IsComment(const wstring& str)
+	{
+		std::wregex pattern(L"^//.*", std::regex_constants::extended);
+		return std::regex_match(str, pattern);
+	}
 }
 
 namespace Inferno
@@ -28,8 +36,10 @@ namespace Inferno
 class Scripter
 {
 public:
-	void LoadSceneFromScript(const std::wstring& filename)
+	static void LoadSceneFromScript(ShooterScene* scene, const Graphics& g, const std::wstring& filename)
 	{
+		//仕様するエンコーディング
+		Encoding encoding = Encoding::e_utf8;
 		//古いロケールを一時保存
 		std::locale oldLocale;
 
@@ -46,30 +56,25 @@ public:
 
 			//エンコーディング判定（bom付きも考慮）
 			//utf8
-			std::regex patternU8("(\xEF\xBB\xBF)?encoding\\(UTF8\\)", 
+			std::regex patternU8("(\xEF\xBB\xBF)?encoding\\(UTF-?8\\)", 
 				std::regex_constants::extended | std::regex_constants::icase);
 			bool resultU8 = std::regex_match(buff, patternU8);
-			if (resultU8) m_encoding = Encoding::e_utf8;
+			if (resultU8) encoding = Encoding::e_utf8;
 			//sjis
 			std::regex patternSJIS("encoding\\(SJIS\\)", 
 				std::regex_constants::extended | std::regex_constants::icase);
 			bool resultSJIS = std::regex_match(buff, patternSJIS);
-			if (resultSJIS) m_encoding = Encoding::e_sjis;
+			if (resultSJIS) encoding = Encoding::e_sjis;
 		}
 
-		//空ロケール -> utf-8
-		const std::locale empty_locale = std::locale::empty();
-		typedef std::codecvt_utf8<char> converter_type;
-		const converter_type* converter = new converter_type;
-		const std::locale utf8_locale = std::locale(empty_locale, converter);
-
-		std::ifstream in;
+		std::wifstream in;
 		//開くエンコーディングを選択
-		switch (m_encoding)
+		switch (encoding)
 		{
 		case Encoding::e_utf8:
 			in.open(filename);
-			in.imbue(utf8_locale); //utf-8で開く
+			//utf-8をutf16で開く
+			in.imbue(std::locale(in.getloc(), new std::codecvt_utf8_utf16<wchar_t>));
 			break;
 
 		case Encoding::e_sjis:
@@ -86,8 +91,8 @@ public:
 			throw GeneralFileError(filename, L"ファイルを開くことに失敗しました");
 		}
 		
-		char buff[1024];
-		match_results<const char*> match;
+		wchar_t buff[1024];
+		match_results<const wchar_t*> match;
 		while (in.good())
 		{
 			//一行読み込む
@@ -96,17 +101,37 @@ public:
 			//コメントは読み飛ばす
 			if (IsComment(buff)) continue;
 
+			//encoding命令は飛ばす
+			{
+				std::wregex pattern(L"(\xEF\xBB\xBF)?encoding\\(.+\\)",
+					std::regex_constants::extended | std::regex_constants::icase);
+				if (std::regex_match(buff, pattern)) continue;
+			}
+
 			//create命令
 			{
-				std::regex pattern("^create\\([:s:]*([^,]+)[:s:]*,[:s:]*([^)]+)[:s:]*\\)$", std::regex_constants::extended);
+				std::wregex pattern(L"^create\\([[:s:]]*([^)]+)[[:s:]]*\\)$", std::regex_constants::extended);
 				if (std::regex_match(buff, match, pattern))
 				{
 					//id
-					string str = match.str(1);
+					scene->CreateEnemy(std::atoi( TString(match.str(1)).GetStringA()));
+					continue;
+				}
+			}
+
+			//loadimage命令
+			{
+				std::wregex pattern(L"^loadimage\\([[:s:]]*([[:d:]]+)[[:s:]]*,[[:s:]]*\"([^\"]+)\"[[:s:]]*\\)$",
+					std::regex_constants::extended);
+				if (std::regex_match(buff, match, pattern))
+				{
+					Texture newTex;
+					//TString使用は応急処置
+					newTex.LoadImageFile(g,match.str(2));
+					scene->SetTexture(std::atoi( TString(match.str(1)).GetStringA() ), newTex);
 				}
 			}
 		}
-
 
 		//古いロケールを読み戻す
 		std::locale::global(oldLocale);
@@ -119,8 +144,6 @@ private:
 		e_utf16,
 		e_utf8,
 	};
-
-	Encoding m_encoding = Encoding::e_utf8;
 };
 
 }

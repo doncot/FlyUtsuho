67cc400a75b98aac5aa6cdcbb14f4626bf57446e
doncot <doncot@gmail.com>
2014-08-13 00:29:20 +0900

    Revert "衝突してない判定も修正"
    This reverts commit 5820cfd57a2c62da2b11078f004ac0949fba0014.

--- Inferno/CollisionDetector.cpp
+++ Inferno/CollisionDetector.cpp
@@ -20,11 +20,15 @@ bool IsPointInsideRect(const Vec2<int>& v, const Rect& r)
 
 bool IsRect1NotHittingRect2(const Rect& r1, const Rect& r2)
 {
-	if ((r1.Left() < r2.Right() || r1.Right() > r2.Left()) ||
-		(r1.Top() < r2.Bottom() || r1.Bottom() > r2.Top()))
-	{
-		return true;
-	}
+	//矩形同士が重なっていない場合を検出
+	//r1がr2の上側にいる
+	if (r1.Bottom() < r2.Top())	return true;
+	//r1がr2の下側にいる
+	else if (r1.Top() > r2.Bottom()) return true;
+	//r1がr2の左側にある
+	else if (r1.Right() < r2.Left()) return true;
+	//r1がr2の右側にある
+	else if (r1.Left() > r2.Right()) return true;
 	else return false;
 }
 
